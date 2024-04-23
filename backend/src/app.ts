82fa931df4b6acc179d30100fac7b9a1e395a6e4
Jon <jon.cooler@yahoo.com>
2024-04-23 08:33:12 -0400

    implementing login routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,10 @@ import express, { NextFunction, Request, Response } from "express";
 import candidateRoutes from "./routes/candidates";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
+import userRoutes from "./routes/users";
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cors = require('cors');
 
@@ -14,6 +18,20 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use(session({
+secret: env.SESSION_SECRET,
+resave: false,
+saveUninitialized: false,
+cookie: {
+  maxAge: 60 * 60 * 1000,
+},
+rolling: true,
+store: MongoStore.create({
+ mongoUrl: env.MONGO_CONNECTION_STRING,
+}),
+}));
+
+app.use("/api/users", userRoutes);
 app.use("/api/candidates", candidateRoutes);
 
 app.use((req, res, next) => {
