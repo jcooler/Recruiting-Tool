d95904090355c6825bd8f268617829868431270d
Jon <jon.cooler@yahoo.com>
2024-04-29 12:07:06 -0400

    updates

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -3,6 +3,6 @@ import { port, str } from "envalid/dist/validators";
 
 export default cleanEnv(process.env, {
   MONGO_CONNECTION_STRING: str(),
-  PORT: port(),
+  PORT: port({ default: 5001}), 
   SESSION_SECRET: str(),
 });
